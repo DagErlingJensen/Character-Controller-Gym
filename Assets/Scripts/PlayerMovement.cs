d0b25e5247d3144a5748f08bf28e75b42404ab58
Eskil Dahl Pettersen <eskil.dahl.pettersen@gmail.com>
2021-12-21 17:13:49 +0100

    Basic FPS controller w/double jump
    Implemented basic FPS controller based on the Brackeys tutorial. Also added double jump.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed = 10;
+    public float GravityMultiplier = 1;
+    public float JumpHeight = 3;
+
+    CharacterController controller;
+
+    public Transform GroundCheck;
+    public LayerMask GroundLayer;
+
+    float gravityConst = -9.81f;
+    public bool isGrounded;
+    public bool CanDoubleJump;
+    public Vector3 velocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, .4f, GroundLayer);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+            if (!CanDoubleJump)
+                CanDoubleJump = true;
+        }
+        else
+            velocity.y += gravityConst * GravityMultiplier * Time.deltaTime;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = transform.right * x + transform.forward * z;
+        Vector3 movement = movementDirection * MovementSpeed * Time.deltaTime;
+        controller.Move(movement);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2 * gravityConst * GravityMultiplier);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && CanDoubleJump)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2 * gravityConst * GravityMultiplier);
+            CanDoubleJump = false;
+        }
+
+        //velocity.y += gravityConst * GravityMultiplier * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
