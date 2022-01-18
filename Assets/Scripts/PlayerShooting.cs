de8526e70e77333ba391b7f85ee3803351ba3b41
Eskil Dahl Pettersen <eskil.dahl.pettersen@gmail.com>
2022-01-18 22:23:29 +0100

    Particle effects

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     public float range = 100;
-
+    public ParticleSystem MuzzleFlash;
+    public GameObject HitEffect;
     Camera fpsCam;
 
     private void Awake()
@@ -21,10 +22,14 @@ public class PlayerShooting : MonoBehaviour
 
     void Shoot()
     {
+        MuzzleFlash.Play();
+
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+            GameObject impactEffect = Instantiate(HitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactEffect, impactEffect.GetComponent<ParticleSystem>().main.duration);
             if (hit.transform.GetComponent<Target>())
                 hit.transform.GetComponent<Target>().Hit();
         }
