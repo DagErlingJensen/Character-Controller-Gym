eb3fec21c0af25f857173edf1f34862ce938263c
Eskil Dahl Pettersen <eskil.dahl.pettersen@gmail.com>
2022-01-20 09:39:07 +0100

    Exported particle effects package

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -7,11 +7,12 @@ public class PlayerShooting : MonoBehaviour
     public float range = 100;
     public ParticleSystem MuzzleFlash;
     public GameObject HitEffect;
-    Camera fpsCam;
 
-    private void Awake()
+    Camera camera;
+
+    void Start()
     {
-        fpsCam = Camera.main;
+        camera = Camera.main;
     }
 
     void Update()
@@ -25,11 +26,13 @@ public class PlayerShooting : MonoBehaviour
         MuzzleFlash.Play();
 
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
             GameObject impactEffect = Instantiate(HitEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactEffect, impactEffect.GetComponent<ParticleSystem>().main.duration);
+
             if (hit.transform.GetComponent<Target>())
                 hit.transform.GetComponent<Target>().Hit();
         }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,6 +7,7 @@ public class Target : MonoBehaviour
 {
     public void Hit()
     {
+        Debug.Log(name + "hit");
         Invoke("OnHit", 0);
     }
 
